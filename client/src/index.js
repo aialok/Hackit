0fd82af013773ebec053e2143c518b10af5bee58
aialok <alokgupta1560@gmail.com>
2024-04-05 02:53:08 +0530

    Added analytics for website

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,13 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 // import { Provider } from "react-redux";
+import { inject } from "@vercel/analytics";
+
+inject();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-      <App />
+  <BrowserRouter>
+    <App />
   </BrowserRouter>
 );
